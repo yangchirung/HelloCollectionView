275b76eb2bc66ea6b21b8d1ca13f172744cf8091
jackyang <jy6231@yahoo.com.tw>
2019-11-30 15:36:06 +0800

    button3

--- HelloCollectionView/ViewController.swift
+++ HelloCollectionView/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
 
     @IBOutlet weak var myCC: UICollectionView!
     var images:[UIImage?] = []
+    var itemsInLine:CGFloat = 3
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,17 +36,22 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         layout.minimumLineSpacing = 5
         let side = screenSize.width / 3 - 10
         layout.itemSize = CGSize(width: side, height: side)
-        
         myCC.setCollectionViewLayout(layout, animated: false)
         
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.rotate),
+                                               name:UIDevice.orientationDidChangeNotification,
+                                               object: nil)
         
-        
-        
-        
+  
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -56,8 +62,30 @@ class ViewController: UIViewController,UICollectionViewDelegate,UICollectionView
         return cell
     }
     
+    @objc func rotate(){
+        print("rotate")
+        let screenSize = UIScreen.main.bounds.size
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        layout.minimumLineSpacing = 10
+        let side = screenSize.width / itemsInLine - 10
+        layout.itemSize = CGSize(width: side, height: side)
+        myCC.setCollectionViewLayout(layout, animated: true)
+    }
     
-  
-
+    @IBAction func buttonAction4(_ sender: Any) {
+        itemsInLine = 4
+        rotate()
+    }
+    
+    @IBAction func buttonAction1(_ sender: Any) {
+        itemsInLine = 2
+        rotate()
+    }
+    
+    @IBAction func buttonAction3(_ sender: Any) {
+        itemsInLine = 3
+        rotate()
+    }
 }
 
